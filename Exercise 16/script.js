cebcfdcbd99640d146b68b0051d093cd4fd35f1f
Pankaj <pankajmaurya1607@gmail.com>
2025-08-30 01:56:32 +0530

    default and name export

---
+++ Exercise 16/script.js
@@ -0,0 +1,8 @@
+// Default Export:
+
+// 1. Write a JavaScript module that exports a default function to greet a user and use it in another file.
+
+
+import gr from "./greet.js"
+
+console.log(gr("Pankaj"))
---
+++ Exercise 17/script.js
@@ -0,0 +1,12 @@
+// Named Exports:
+
+// 2. Write a JavaScript module that exports multiple named functions for basic arithmetic operations.
+
+import {add, sub, mul, div} from "./arithmetic_ops.js"
+
+console.log(add(2, 5));
+console.log(sub(7, 5));
+console.log(mul(10, 5));
+console.log(div(10, 5));
+
+
